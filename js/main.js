7a519fe3876a1bc9a70b22db82bcbba31fd26ceb
jjya1b2c3vu <49565322+jjya1b2c3vu@users.noreply.github.com>
2020-09-29 05:57:35 +0900

    ga적용 최종끝

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ function checkPswd() {
     var confirmPassword = "20"; 
     var password = document.getElementById("pswd").value; 
     if (password === confirmPassword) { 
-        alert("Passwords matched");
-        window.location.replace("./page.html");
+        // alert("Passwords matched");
+        window.location.href="./page.html";
     } 
     else{ 
         alert("Passwords do not match."); 
