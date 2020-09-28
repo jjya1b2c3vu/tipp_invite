f7bf4805514e8229031765f54492a6b034d436c3
jjya1b2c3vu <49565322+jjya1b2c3vu@users.noreply.github.com>
2020-09-29 01:40:51 +0900

    경고창순서조정

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ function checkPswd() {
     var confirmPassword = "20"; 
     var password = document.getElementById("pswd").value; 
     if (password == confirmPassword) { 
-        window.location.href="page.html";
         alert("Passwords matched");
+        window.location.href="page.html";
     } 
     else{ 
         alert("Passwords do not match."); 
