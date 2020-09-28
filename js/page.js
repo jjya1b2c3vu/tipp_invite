7dd6d1b27c1fa5032af356bbe8aedc1ae35de8d3
jjya1b2c3vu <49565322+jjya1b2c3vu@users.noreply.github.com>
2020-09-29 00:12:34 +0900

    반응형 및 인터렉티브 스크롤 개선

--- js/page.js
+++ js/page.js
@@ -1,9 +1,9 @@
 window.onscroll = function() {
     myFunction();
 };
-
+var p = 1000;
 function myFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+  if (document.body.scrollTop > p || document.documentElement.scrollTop > p) {
     document.getElementById("myP0").className ="mark";
     document.getElementById("right-side-0").className ="mark";
   }
@@ -12,7 +12,7 @@ function myFunction() {
     document.getElementById("right-side-0").className ="no_mark";
   }
 
-  if(document.body.scrollTop > 900 || document.documentElement.scrollTop > 900){
+  if(document.body.scrollTop > 2*p || document.documentElement.scrollTop > 2*p){
     document.getElementById("myP1").className = "mark";
     document.getElementById("right-side-1").className ="mark";
     document.getElementById("myP0").className ="no_mark";
@@ -23,7 +23,7 @@ function myFunction() {
     document.getElementById("right-side-1").className ="no_mark";
   }
 
-  if(document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500){
+  if(document.body.scrollTop > 3*p || document.documentElement.scrollTop > 3*p){
     document.getElementById("myP2").className = "mark";
     document.getElementById("right-side-2").className = "mark";
     document.getElementById("myP1").className ="no_mark";
@@ -33,7 +33,7 @@ function myFunction() {
     document.getElementById("myP2").className ="no_mark";
     document.getElementById("right-side-2").className = "no_mark";
   }
-  if(document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100){
+  if(document.body.scrollTop > 4*p || document.documentElement.scrollTop > 4*p){
     document.getElementById("myP3").className = "mark";
     document.getElementById("right-side-3").className = "mark";
     document.getElementById("myP2").className ="no_mark";
@@ -44,7 +44,7 @@ function myFunction() {
     document.getElementById("right-side-3").className = "no_mark";
   }
 
-  if(document.body.scrollTop > 2900 || document.documentElement.scrollTop > 2900){
+  if(document.body.scrollTop > 5*p || document.documentElement.scrollTop > 5*p){
     document.getElementById("myP4").className = "mark";
     document.getElementById("right-side-4").className = "mark";
     document.getElementById("myP3").className ="no_mark";
@@ -55,3 +55,5 @@ function myFunction() {
     document.getElementById("right-side-4").className = "no_mark";
   }
 }
+
+
